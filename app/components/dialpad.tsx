6c3e90b89b089e65b4f4fee6bdff738e5adc74da
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-04-27 18:52:11 +0300

    Update dialpad.tsx

--- app/components/dialpad.tsx
+++ app/components/dialpad.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { cn } from "~/lib/cn";
 import { useDialPadContext } from "~/lib/context/dialpad";
 import { getCurrencySymbol } from "~/utils/helpers";
@@ -9,7 +10,7 @@ export const DialPad = () => {
       <DialPadRow first="1" second="2" third="3" />
       <DialPadRow first="4" second="5" third="6" />
       <DialPadRow first="7" second="8" third="9" />
-      <DialPadRow first="." idFirst="dot" second="0" third="<" idThird="back" />
+      <DialPadRow first="." second="0" third="<" idThird="Backspace" />
     </div>
   );
 };
@@ -34,9 +35,9 @@ const DialPadRow = ({
   return (
     <ul>
       <div className="flex justify-between">
-        <DialPadKey label={first} id={idFirst} />
-        <DialPadKey label={second} id={idSecond} />
-        <DialPadKey label={third} id={idThird} />
+        <DialPadKey label={first} id={idFirst ? idFirst : first} />
+        <DialPadKey label={second} id={idSecond ? idSecond : second} />
+        <DialPadKey label={third} id={idThird ? idThird : third} />
       </div>
     </ul>
   );
@@ -45,33 +46,66 @@ DialPadRow.displayName = "DialPadRow";
 
 type DialPadKeyProps = {
   label: string;
-  id?: string;
+  id: string;
 };
 const DialPadKey = ({ label, id }: DialPadKeyProps) => {
   const { amountValue, setAmountValue } = useDialPadContext();
+  const idsList = [
+    "Backspace",
+    ".",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  useEffect(() => {
+    function handleKeyDown(e: any) {
+      handleDialPadInputs(e.key);
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amountValue]);
+
+  const handleDialPadInputs = (id: string) => {
+    const label = id === "Backspace" ? "<" : id;
+    if (idsList.indexOf(id) !== -1) {
+      if (id === "Backspace") {
+        setAmountValue(`${amountValue.substring(0, amountValue.length - 1)}`);
+      } else if (amountValue === "0" && id !== ".") {
+        setAmountValue(
+          `${amountValue.substring(0, amountValue.length - 1)}${label}`
+        );
+      } else if (
+        (id === "." &&
+          amountValue.indexOf(".") === -1 &&
+          amountValue.length !== 0) ||
+        id !== "."
+      ) {
+        setAmountValue(`${amountValue}${label}`);
+      }
+    }
+  };
+
   return (
     <li
       className={cn(
         "cursor-pointer hover:text-green-1",
-        id === "dot" ? "pl-1" : ""
+        id === "." ? "pl-1" : ""
       )}
-      id={id ? id : `nr-${label}`}
-      onClick={() => {
-        if (id === "back") {
-          setAmountValue(`${amountValue.substring(0, amountValue.length - 1)}`);
-        } else if (amountValue === "0" && id !== "dot") {
-          setAmountValue(
-            `${amountValue.substring(0, amountValue.length - 1)}${label}`
-          );
-        } else if (
-          (id === "dot" &&
-            amountValue.indexOf(".") === -1 &&
-            amountValue.length !== 0) ||
-          id !== "dot"
-        ) {
-          setAmountValue(`${amountValue}${label}`);
-        }
-      }}
+      tabIndex={0}
+      id={id}
+      onClick={() => handleDialPadInputs(id)}
     >
       {label}
     </li>
