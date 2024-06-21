eba62505be1b0eb140890838c1f0a718f3a74ac3
Tymmmy <117268143+Tymmmy@users.noreply.github.com>
2024-06-21 15:04:22 +0300

    Fix dialpad numbers input

--- app/components/dialpad.tsx
+++ app/components/dialpad.tsx
@@ -3,7 +3,7 @@ import { cn } from "~/lib/cn";
 import { useDialPadContext } from "~/lib/context/dialpad";
 import { getCurrencySymbol } from "~/utils/helpers";
 
-enum DialPadIds {
+export enum DialPadIds {
   Backspace = "Backspace",
   Dot = ".",
   Zero = "0",
